de8e32368a24637570ef5cac3a62858278c95caf
tim the fiend <tim@okoroko.com>
2022-10-13 13:52:17 +0200

    shit works

--- main.py
+++ main.py
@@ -1,55 +1,83 @@
-import os
 from datetime import datetime
-import threading
+from multiprocessing.pool import ThreadPool
 import time
-import sys
+import os
 
-import requests as req
+from constants import *
+
+import requests
 import PyPDF2
 import pandas as pd
-
-import remove_files
-from constants import *
 
 ID_COL = None
 URL_COL = None
 URL_BACKUP = None
-DONE = False
+
+MAX_ROWS = None
+ROWS_DONE = None
+
+def remove_invalid_files():
+    file_list = []
+    with open(OUTPUT_FILE_SUCCESS, 'r') as myfile:
+        while True:
+            line = myfile.readline()[:-1]
+
+            if line == "":
+                break
+            if line == "\n":
+                continue
+            file_list.append(line)
+
+    for x in os.listdir(OUTPUT_PATH):
+        file, ext = os.path.splitext(x)
+        if file not in file_list:
+            os.remove(os.path.join(OUTPUT_PATH, x))
 
 
-def on_start_up():
-    _output = open(OUTPUT_FILE, 'w')
-    _output.close()
-
-    if CLEAN_OUTPUT:
-        clean_output()
+def check_if_skip(id: str) -> bool:
+    return f"{id}.pdf" in os.listdir(OUTPUT_PATH)
 
 
-def report_result(id : str, file_name : str) -> None:
+
+def report_success(id : str, file_name : str) -> None:
     def success():
-        with open(OUTPUT_FILE, 'a') as myfile:
+        with open(OUTPUT_FILE_SUCCESS, 'a') as myfile:
             myfile.write(id + "\n")
+
     try:
         PyPDF2.PdfFileReader(open(file_name, 'rb'), strict=False)
         success()
     except:
+        #Not sure this does anything.
+        report_failure(id)
         os.remove(file_name)
         return
 
-def loading_animation():
-    import itertools
-
-    for c in itertools.cycle(['|', '/', '-', '\\']):
-        if DONE:
-            break
-        sys.stdout.write('\rloading ' + c)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rDone!     ')
+def report_failure(id : str) -> None:
+    with open(OUTPUT_FILE_FAIL, 'a') as myfile:
+        myfile.write(f"\t{id} \n")
 
 
-def get_names(_start: int, _end: int, _id: int):
-    def is_not_url(url_str : str) -> bool:
+def initialise_columns():
+    print(f"{datetime.now().strftime('%H:%M:%S')}\tLoading spreadsheet.")
+    sheet = pd.ExcelFile(FILE_PATH).parse(0)
+
+    # Columns
+    global ID_COL
+    global URL_COL
+    global URL_BACKUP
+
+    ID_COL = sheet[ID_COL_NAME]
+    URL_COL = sheet[URL_COL_NAME]
+    URL_BACKUP = sheet[URL_BACKUP_COL_NAME]
+
+    global MAX_ROWS
+    MAX_ROWS = int(ID_COL.size)
+    print(f"{datetime.now().strftime('%H:%M:%S')}\tFinished loading spreadsheet.")
+
+
+def get_pdf(row_id: int) -> None:
+    def is_not_url(url_str: str) -> bool:
         return not url_str.lower().startswith('http')
 
     def on_success(pdf) -> None:
@@ -61,95 +89,71 @@ def get_names(_start: int, _end: int, _id: int):
                 os.remove(file_name)
                 return
             finally:
-                report_result(id, file_name)
+                report_success(id, file_name)
 
-    midway_point = int((_end - _start) / 2) + _start
+    url = str(URL_COL[row_id])
+    url_backup = str(URL_BACKUP[row_id])
+    id = ID_COL[row_id]
 
-    for i in range(_start, _end):
-        if i == midway_point:
-            print(f"{_id} halfway there")
-            sys.stdout.flush()
+    if check_if_skip(id):
+        return
 
-        url = str(URL_COL[i])
-        url_backup = str(URL_BACKUP[i])
-        id = ID_COL[i]
+    for _url in [url, url_backup]:
+        if is_not_url(_url):
+            return
+        pdf = requests.get(url=_url, allow_redirects=True,
+                        timeout=TIMEOUT_REQUEST_MAX)
+        pdf.raise_for_status()
 
-        # Check if valid URL address.
-        for item in [url, url_backup]:
-            if is_not_url(item):
-                continue
-            try:
-                pdf = req.get(url=url, allow_redirects=True, timeout=TIMEOUT_REQUEST_MAX, stream=True)
-                pdf.raise_for_status()
-                if pdf.status_code == 200:
-                    on_success(pdf)
-                    break
-            except:
-                pass
+        # attempt_status = False
+        if pdf.status_code == 200:
+            on_success(pdf)
+            return
 
 
 
+def process_get_pdf():
+    pool = ThreadPool(processes=NUMBER_OF_THREADS)
+    # _tasks = []
+    # for index, row in enumerate(ID_COL):
+    #     _tasks.append(pool.apply_async(get_pdf, args=(row, URL_COL[index], URL_BACKUP[index], )))
 
-def main():
-    #loading
-    loading = threading.Thread(target=loading_animation)
-    loading.start()
+    for i in range(MAX_ROWS):
+        pool.apply_async(get_pdf, (i, ))
 
-    sheet = pd.ExcelFile(FILE_PATH).parse(0)
-    print("EXCEL HAS BEEN LOADED")
-
-    s = time.perf_counter()
-    # Columns
-    global ID_COL
-    global URL_COL
-    global URL_BACKUP
-    global DONE
-    ID_COL = sheet[ID_COL_NAME]
-    URL_COL = sheet[URL_COL_NAME]
-    URL_BACKUP = sheet[URL_BACKUP_COL_NAME]
-
-    assert type(ID_COL) == pd.core.series.Series
-    assert type(URL_COL) == pd.core.series.Series
-    assert type(URL_BACKUP) == pd.core.series.Series
-
-
-    total_row_count = int(ID_COL.size / CAP_TOTAL_AMOUNT_OF_ROWS)
-    amount = int(total_row_count / NUMBER_OF_THREADS)
-
-    # print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-    print(f"Estimated time: {(((total_row_count / NUMBER_OF_THREADS) * TIMEOUT_REQUEST_MAX) / 60) * 2.5:0.2f}min")
-
-    threads = []
-
-    for x in range(NUMBER_OF_THREADS):
-        start_value = x * amount
-        end_value = amount * (x + 1)
-        if x == NUMBER_OF_THREADS - 1:
-            end_value += 1
-
-        t = threading.Thread(target=get_names, name=f"T{x + 1}", args=(start_value, end_value, x,))
-        threads.append(t)
-
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
-
-    elapsed = time.perf_counter() - s
-    DONE = True
-    print(f"Executed in {elapsed / 60:0.2f}min.")
-
+    pool.close()
+    pool.join()
 
 def clean_output() -> None:
     for x in os.listdir(OUTPUT_PATH):
         os.remove(os.path.join(OUTPUT_PATH, x))
 
+def create_result_files():
+    if not len(os.listdir(OUTPUT_PATH)) > 0:
+        foo = open(OUTPUT_FILE_SUCCESS, 'w')
+        foo.close()
+    _output = open(OUTPUT_FILE_FAIL, 'w')
+    _output.close()
+
+
+def main():
+    create_result_files()
+
+    initialise_columns()
+    if CLEAN_OUTPUT:
+        clean_output()
+
+    s = time.perf_counter()
+    print(f"{datetime.now().strftime('%H:%M:%S')}\tEstimated time: {(((MAX_ROWS / NUMBER_OF_THREADS) * TIMEOUT_REQUEST_MAX) / 60):0.2f}min")
+
+    process_get_pdf()
+
+    elapsed = time.perf_counter() - s
+    print(f"{datetime.now().strftime('%H:%M:%S')}\tExecuted in {elapsed / 60:0.2f}min.")
+
+    remove_invalid_files()
+
+
 
 if __name__ == "__main__":
-    on_start_up()
-    print(f"START:\t{datetime.now().strftime('%H:%M:%S')}")
-    # print(f"START:\t{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     main()
-    remove_files.remove_invalid_files()
-    print(f"END:\t{datetime.now().strftime('%H:%M:%S')}")
-    # print(f"END:\t{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
